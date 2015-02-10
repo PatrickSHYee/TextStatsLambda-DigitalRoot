ccec3c60c19aae9f958a84bf0bc5e7d90580140a
PatrickSHYee <patrickshyee@gmail.com>
2015-02-10 00:18:59 -0700

    Fun learn and programming about lambda expressions
    man, this could been helpful in data structures when I took it in 2003

--- TextStatsLambda-DigitalRoot/TextStatsLambda-DigitalRoot/Program.cs
+++ TextStatsLambda-DigitalRoot/TextStatsLambda-DigitalRoot/Program.cs
@@ -10,43 +10,93 @@ namespace TextStatsLambda_DigitalRoot
     {
         static void Main(string[] args)
         {
-
+            string temp = "Mike's favorite color is blue.";
+            Console.WriteLine("Consonants: {0}", NumberOfConsonants(temp));
+            Console.WriteLine("Special Chars: {0}", NumberOfSpecialCharacters(temp));
         }
 
+        /// <summary>
+        /// Sum of the digits.
+        /// </summary>
+        /// <param name="rootThisNumber">string of digits</param>
+        /// <returns>returns int < 10</returns>
         public static int DigitalRoot(string rootThisNumber)
         {
-            return 0;
+            int returnInt;
+            try
+            {
+                int.TryParse(rootThisNumber, out returnInt);
+                return (1 + (returnInt - 1) % 9);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid entry");
+                return 0;
+            }
         }
 
+        /// <summary>
+        /// Number of the words in a sentence
+        /// </summary>
+        /// <param name="inputString">a sentence</param>
+        /// <returns>number of words</returns>
         public static int NumberOfWords(string inputString)
         {
-            return 0;
+            List<string> words = inputString.Split(' ').ToList<string>();
+            return words.Count;
         }
 
+        /// <summary>
+        /// Number of vowels in a sentence
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
         public static int NumberOfVowels(string inputString)
         {
-            return 0;
+            var vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            return inputString.Count(x => vowels.Contains(x));
         }
 
+        /// <summary>
+        /// Number of Consonants in a sentence remember it's case dependent
+        /// </summary>
+        /// <param name="inputString">a sentence</param>
+        /// <returns>number of consonants</returns>
         public static int NumberOfConsonants(string inputString)
         {
-            return 0;
+            // bcdfghjklmnpqrstvwxyz
+            return inputString.Count(x => "bcdfghjklmnpqrstvwxyz".Contains(x.ToString().ToLower()));
         }
 
+        /// <summary>
+        /// number of special characters in a sentence
+        /// </summary>
+        /// <param name="inputString">a sentence</param>
+        /// <returns>nubmer of special characters</returns>
         public static int NumberOfSpecialCharacters(string inputString)
         {
             // .,?;'!@#$%^&*() and spaces are considered special characters
-            return 0;
+            return inputString.Count(x => " .,?;'!@#$%^&*()".Contains(x));
         }
 
+        /// <summary>
+        /// Order by descending to find the longest word
+        /// </summary>
+        /// <param name="inputString">a sentence</param>
+        /// <returns>longest word</returns>
         public static string LongestWord(string inputString)
         {
-            return string.Empty;
+            return inputString.Split(' ').OrderByDescending(x=>x.Length).First();
         }
 
+        /// <summary>
+        /// Using the order by descending to find the stortest word
+        /// </summary>
+        /// <param name="inputString">a sentence</param>
+        /// <returns>the shortest word</returns>
         public static string ShortestWord(string inputString)
         {
-            return string.Empty;
+            return inputString.Split(' ').OrderByDescending(x=>x.Length).Last();
         }
 
 
